95c35c56aec2f180463da335f5240f1022f22a69
Anzhelika Susemcik <angelika.tyrnova@gmail.com>
2022-12-08 18:43:51 -0500

    added Bootstrap

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from "react";
 
 export default function App() {
   return (
-    <div>
-      <h3>Weather App</h3>
+    <div className="App">
+      <div className="container">
+        <h3>Weather App</h3>
+      </div>
       <footer>
         <a
           href="https://github.com/AnzhelikaTy/react-weather-project.git"
