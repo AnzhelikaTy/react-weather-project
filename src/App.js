70aa2b50f13d312a0b7227d1c4187a239c70c7d5
Anzhelika Susemcik <angelika.tyrnova@gmail.com>
2022-12-08 18:51:13 -0500

    imported Weather.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from "react";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         <h3>Weather App</h3>
+        <Weather />
+        <footer>
+          <a
+            href="https://github.com/AnzhelikaTy/react-weather-project.git"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>{" "}
+          by Anzhelika Susemcik
+        </footer>
       </div>
-      <footer>
-        <a
-          href="https://github.com/AnzhelikaTy/react-weather-project.git"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-source code
-        </a>{" "}
-        by Anzhelika Susemcik
-      </footer>
     </div>
   );
 }
