5c44f6d1b50732abe813a8bdda44e9f3f1b0748b
Anzhelika Susemcik <angelika.tyrnova@gmail.com>
2022-12-08 19:33:44 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,19 +5,18 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h3>Weather App</h3>
         <Weather />
-        <footer>
-          <a
-            href="https://github.com/AnzhelikaTy/react-weather-project.git"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Open-source code
-          </a>{" "}
-          by Anzhelika Susemcik
-        </footer>
       </div>
+      <footer>
+        <a
+          href="https://github.com/AnzhelikaTy/react-weather-project.git"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>{" "}
+        by Anzhelika Susemcik
+      </footer>
     </div>
   );
 }
