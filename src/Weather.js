482745cdc9cb649a2410440b872726fa5f264a80
Anzhelika Susemcik <angelika.tyrnova@gmail.com>
2022-12-09 21:17:53 -0500

    added styles with Bootstrap; added responsive for small screens with Bootstrap

--- src/Weather.js
+++ src/Weather.js
@@ -6,8 +6,7 @@ export default function Weather() {
     <div className="Weather">
       <form>
         <div className="row">
-          <div className="col-3"></div>
-          <div className="col-4">
+          <div className="col-md-6 mb-2 mb-md-0">
             <input
               type="search"
               placeholder="Enter a city.."
@@ -15,32 +14,31 @@ export default function Weather() {
               autoFocus="on"
             />
           </div>
-          <div className="col-3">
+          <div className="col-md-6">
             <input type="submit" value="Search" className="btn btn-primary" />
           </div>
         </div>
       </form>
       <div className="row">
-        <div className="col-2">
+        <div className="col-md-4 text-center">
           <img
             src="https://ssl.gstatic.com/onebox/weather/48/partly_cloudy.png"
             alt="Mostly Cloudy"
           />
-          6°C
+          <span>6°C</span>
         </div>
-        <div className="col-4">
+        <div className="col-md-4 text-center mt-3 mt-md-0">
+          <h1>New York, USA</h1>
+          <ul>
+            <li>Friday 19:00</li>
+            <li>Mostly Cloudy</li>
+          </ul>
+        </div>
+        <div className="col-md-4 text-center mt-3 mt-md-0">
           <ul>
             <li>Precipitation: 1%</li>
             <li>Humidity: 59%</li>
             <li>Wind: 11km/h</li>
-          </ul>
-        </div>
-        <div className="col-3"></div>
-        <div className="col-3">
-          <h3>New York, USA</h3>
-          <ul>
-            <li>Friday 19:00</li>
-            <li>Mostly Cloudy</li>
           </ul>
         </div>
       </div>
