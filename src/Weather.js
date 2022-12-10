bdc3dcc91d24ec3ba19a206e1c1d14a8290d69b2
Anzhelika Susemcik <angelika.tyrnova@gmail.com>
2022-12-10 14:59:31 -0500

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -6,7 +6,7 @@ export default function Weather() {
     <div className="Weather">
       <form>
         <div className="row">
-          <div className="col-md-6 mb-2 mb-md-0">
+          <div className="col-md-10 mb-2 mb-md-0">
             <input
               type="search"
               placeholder="Enter a city.."
@@ -14,8 +14,12 @@ export default function Weather() {
               autoFocus="on"
             />
           </div>
-          <div className="col-md-6">
-            <input type="submit" value="Search" className="btn btn-primary" />
+          <div className="col-md-2">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -25,7 +29,8 @@ export default function Weather() {
             src="https://ssl.gstatic.com/onebox/weather/48/partly_cloudy.png"
             alt="Mostly Cloudy"
           />
-          <span>6°C</span>
+          <span className="temperature">6°</span>
+          <span className="unit">C</span>
         </div>
         <div className="col-md-4 text-center mt-3 mt-md-0">
           <h1>New York, USA</h1>
@@ -34,11 +39,17 @@ export default function Weather() {
             <li>Mostly Cloudy</li>
           </ul>
         </div>
-        <div className="col-md-4 text-center mt-3 mt-md-0">
+        <div className="col-md-4 text-center mt-3 mt-md-0 conditions">
           <ul>
-            <li>Precipitation: 1%</li>
-            <li>Humidity: 59%</li>
-            <li>Wind: 11km/h</li>
+            <li>
+              <span className="strong">Precipitation:</span> 1%
+            </li>
+            <li>
+              <span className="strong">Humidity:</span> 59%
+            </li>
+            <li>
+              <span className="strong">Wind:</span> 11km/h
+            </li>
           </ul>
         </div>
       </div>
