1bdb219184616d7998108a075291703344893f35
Anzhelika Susemcik <angelika.tyrnova@gmail.com>
2022-12-11 22:04:02 -0500

    set the default city to New York

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="New York" />
       </div>
       <footer>
         <a
