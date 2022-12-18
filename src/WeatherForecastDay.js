d4e60eece8c7e741a21a5237e72ba7910333298e
Anzhelika Susemcik <angelika.tyrnova@gmail.com>
2022-12-18 18:12:25 -0500

    added function day to format the day

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -1,9 +1,17 @@
 import React from "react";
 
 export default function WeatherForecastDay(props) {
+  function day() {
+    let date = new Date(props.data.time * 1000);
+    let day = date.getDay();
+
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+  }
   return (
     <div>
-      <div className="forecast-day">{props.data.dt}</div>
+      <div className="forecast-day">{day()}</div>
       Icon
       <div className="forecast-temperatures">
         <span className="forecast-temperature-max">
