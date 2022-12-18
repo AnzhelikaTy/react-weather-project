9609e5794f997add8eaff5ae26ea14e4b25cdc35
Anzhelika Susemcik <angelika.tyrnova@gmail.com>
2022-12-18 18:59:24 -0500

    Update WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -23,7 +23,7 @@ export default function WeatherForecast(props) {
           {forecast.map(function (dailyForecast, index) {
             if (index < 6) {
               return (
-                <div className="col" key={index}>
+                <div className="col-md" key={index}>
                   <WeatherForecastDay data={dailyForecast} />
                 </div>
               );
